c5dde522948911fd12722daf87f5ec4ed6bfeaa3
Wesley van Son <wesleyvson@hotmail.com>
2020-04-29 00:43:09 +0200

    Home page and footer

--- my-app/ClientApp/src/components/Footer.tsx
+++ my-app/ClientApp/src/components/Footer.tsx
@@ -1,32 +1,34 @@
 import React, { Component } from 'react';
-import { Container } from 'reactstrap';
+import { Container, Navbar } from 'reactstrap';
 import ReactDOM from 'react-dom';
 import { SocialIcon } from 'react-social-icons';
 
 import './Footer.css';
 
 type props = {};
-type state = { collapsed : boolean}
+type state = { collapsed: boolean }
 
 export class Footer extends Component<props, state> {
   static displayName = Footer.name;
 
-  constructor (props: any) {
+  constructor(props: any) {
     super(props);
   }
 
 
-  render () {
+  render() {
     return (
       <footer>
+        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-top box-shadow mb-3" light>
           <Container>
-            <ul className= "footer_bar">
-                <SocialIcon  url="http://twitter.com/jaketrent" />
-                <SocialIcon  url="https://www.linkedin.com/in/wesley-van-son-3a2201189/" />
-                <SocialIcon  url="https://www.instagram.com/wesleyvanson/" />
-                <SocialIcon  url="https://github.com/wesleyvson" />
+            <ul className="footer_bar">
+              <SocialIcon url="http://twitter.com/jaketrent" />
+              <SocialIcon url="https://www.linkedin.com/in/wesley-van-son-3a2201189/" />
+              <SocialIcon url="https://www.instagram.com/wesleyvanson/" />
+              <SocialIcon url="https://github.com/wesleyvson" />
             </ul>
           </Container>
+        </Navbar>
       </footer>
     );
   }
--- my-app/ClientApp/src/components/Home.tsx
+++ my-app/ClientApp/src/components/Home.tsx
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
+import { Container, ListGroup, ListGroupItem, Row, Col } from 'reactstrap';
+import Plaatje1 from '../Images/img1.jpg'
+
+import './Home.css';
 
 export class Home extends Component {
   static displayName = Home.name;
 
-  render () {
+  render() {
     return (
-      <div>
-        <h1>Wesley van Son</h1>
-        <p>verhaaltje over opleiding en website </p>
-      </div>
+      <Container>
+        <Row>
+          <Col><h1>Portfolio Wesley van Son</h1></Col>
+        </Row>
+        <Row>
+          <Col> <p>Welkom op mijn website. Op deze website is alle informatie te vinden over mij. Denk hierbij aan: </p></Col>
+        </Row>
+        <Row>
+          <Col>
+            <div className="content_list">
+              <ListGroup flush>
+                <ListGroupItem>Wie ben ik?</ListGroupItem>
+                <ListGroupItem>Opleiding</ListGroupItem>
+                <ListGroupItem>Ervaringen</ListGroupItem>
+                <ListGroupItem>Projecten</ListGroupItem>
+                <ListGroupItem>Curriculum Vitae</ListGroupItem>
+              </ListGroup>
+            </div>
+          </Col>
+          <Col>
+            <div className="home-image">
+              <img src={Plaatje1} />
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
